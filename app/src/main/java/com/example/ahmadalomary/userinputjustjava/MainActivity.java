2d477d06e18b02682f4a3e7a22b400a484e9e08f
ahmad.alomary <ahmad.alomary@autotrader.co.uk>
2017-01-03 12:11:52 +0000

    2

--- app/src/main/java/com/example/ahmadalomary/userinputjustjava/MainActivity.java
+++ app/src/main/java/com/example/ahmadalomary/userinputjustjava/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.ahmadalomary.userinputjustjava;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+    import android.os.Bundle;
+    import android.support.v7.app.AppCompatActivity;
+    import android.view.View;
+    import android.widget.TextView;
+
+    import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    int total=0;
+    int totalPrice=0;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+
+        public void submitOrder(View view) {
+            display(1);
+            displayPrice();
+        }
+
+        private void display(int number) {
+            TextView quantityTextView = (TextView) findViewById(R.id.quantityTextView);
+            total += number;
+            quantityTextView.setText("" + total);
+        }
+
+        private void displayPrice() {
+            TextView priceTextView = (TextView) findViewById(R.id.priceTextView);
+            totalPrice = total * 5;
+            priceTextView.setText(NumberFormat.getCurrencyInstance().format(totalPrice));
+        }
     }
-}
+
